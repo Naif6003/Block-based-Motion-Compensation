717752ab09b99adbdd2b2a7565b8d8b0ca2fc2fe
Naif Said <naifsaid@naifs-mbp.attlocal.net>
2018-04-28 18:54:29 -0700

    image edges are not working, motion vector is working.

--- hw-4/src/CS4551_Said.java
+++ hw-4/src/CS4551_Said.java
@@ -1,7 +1,7 @@
 import java.awt.FileDialog;
 import java.awt.Frame;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class CS4551_Said {
@@ -41,7 +41,7 @@ public class CS4551_Said {
 		
 		System.out.print("Choose the block size (8, 16, or 24): "); 
 		int blockSize = sc.nextInt();
-		System.out.print("Choose the search size (4, 8, 12, or 16):");
+		System.out.print("Choose the search size (4, 8, 12, or 16): ");
 		int searchIndex = sc.nextInt();
 
 		imageExtracting(targetImg, referenceImg, blockSize, searchIndex);
@@ -70,16 +70,16 @@ public class CS4551_Said {
 	
 	static void compareImages(ArrayList<MiniImage> imageMiniBlockList, Image referenceImg, int p, int n) {
 		
-		int numberOfComparison = (2*p+1);
-		
+		Image finalImage = new Image(referenceImg.getW(),referenceImg.getH());
 		//######### loop over the array of the target image blocks.
 		for(int i = 0; i < imageMiniBlockList.size() ; i++) {
 			
+			ArrayList<MSD> listOfMSDForBlocks = new ArrayList<MSD>();
 			// the position of the Width and Height of the Target block.
 			int width = imageMiniBlockList.get(i).getWidth();
 			int height = imageMiniBlockList.get(i).getHeight();
 			Image blockImg = imageMiniBlockList.get(i).getImg();
-			int targetBlockSum = 0;
+			
 			int[] blockRGB = new int[3];
 			for(int w = 0; w < n; w++) {
 				for(int h = 0; h < n; h++) {
@@ -100,26 +100,219 @@ public class CS4551_Said {
 			for(int x = 0; x < (2*p+1); x++) {
 				for(int y=0; y < (2*p+1) ; y ++) {
 							
-					// one block that is n distance far from the target block
+								// one block that is n distance far from the target block
+								int[] rgb = new int[3];
+								Image comparisonBlocks = new Image(n,n);
+								int miniBlockWidth = 0, miniBlockHeight = 0;
+								
 								for(int w = searchWidthStarts ; w < searchWidthStarts+n ; w++) {
-									System.out.print(w + " ");
+//									System.out.print(w + " ");
 									for(int h = searchHeightStarts ; h < searchHeightStarts+n ; h++) {
-										    System.out.print(h + " ");
+//										    System.out.print(h + " ");
+									
+										if(w < referenceImg.getW() && h < referenceImg.getH()) {  // upper bound
+
+												referenceImg.getPixel(w, h, rgb); 				//convert each block to gray scale
+												int grayBlockValues = (int) Math.round(0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]);
+												int[] grayReferenceBlock = new int[3];
+												grayReferenceBlock[0] = grayBlockValues; grayReferenceBlock[1] = grayBlockValues; grayReferenceBlock[2] = grayBlockValues; 
+												comparisonBlocks.setPixel(miniBlockWidth, miniBlockHeight++, grayReferenceBlock);
+										}
 									}
-									System.out.println();
+									miniBlockWidth++;
+									miniBlockHeight=0;
+//									System.out.println();
 								} // one block loop ends
+								
+								// calculate the MSD for all the reference blocks and add the MSD to an array of all the MSD values
+								// searchWidthStarts is the Width position for every search block, same for searchHeightStarts.
+								int msdValue = calculateTheMSD(comparisonBlocks,blockImg);  // difference in gray value.
+								System.out.println(searchWidthStarts + " " + searchHeightStarts + " " + msdValue);
+								MSD msdForABlock = globalObj.new MSD(searchWidthStarts,searchHeightStarts,msdValue);
+								listOfMSDForBlocks.add(msdForABlock);
+								
 								
 					searchHeightStarts++;
 				}
 				searchWidthStarts++;
 				searchHeightStarts = height-p;
-			}
+				while(searchHeightStarts<0) { searchHeightStarts++; };
+			} // end of the search blocks around the target block position.
 		
+			
+			
+
+			// find the smallest value in the list of MSD for one Target block.
+			
+			int minMSDValue = findMin(listOfMSDForBlocks);
+			
+			
+			System.out.println("  Minimum Value: " + minMSDValue);
+			int msdWidth = 0;
+			int msdHeight = 0;
+			Image errorBlockImg = null;
+			
+			for(int j = 0; j<listOfMSDForBlocks.size(); j++) {
+				if(listOfMSDForBlocks.get(j).getValue() == minMSDValue) {                // finding the best block and its position (w,h)
+					 msdWidth = listOfMSDForBlocks.get(j).getWidth();
+					 msdHeight = listOfMSDForBlocks.get(j).getHeight();
+//					System.out.println(listOfMSDForBlocks.get(j).getValue() + " " + listOfMSDForBlocks.get(j).getWidth() + " " + listOfMSDForBlocks.get(j).getHeight());
+					 
+					 // create best block image from the reference image
+					 int[] rgb = new int[3];
+					 Image bestBlockImage = new Image(n,n);
+					 int xwidth = 0;
+					 int yheight = 0;
+					 for(int x=msdWidth; x < msdWidth+n ; x++){
+						 for(int y = msdHeight; y < msdHeight+n ; y++) {
+//							 System.out.println(x + " x " + y + " y");
+							 if(x < referenceImg.getW() && y < referenceImg.getH()) {
+								 referenceImg.getPixel(x, y, rgb);
+								 int grayBlockValues = (int) Math.round(0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]);
+								 int[] grayReferenceBlock = new int[3];
+								 grayReferenceBlock[0] = grayBlockValues; grayReferenceBlock[1] = grayBlockValues; grayReferenceBlock[2] = grayBlockValues; 
+								 bestBlockImage.setPixel(xwidth, yheight++, grayReferenceBlock);
+							 }
+						 }
+						 xwidth++;
+						 yheight=0;
+					 }
+					 
+					 
+					 /*
+					  * Also, compute the error block (a.k.a. residual block) consisting of pixel differences 
+					  * (absolute values) between the target block and the best matched block (a.k.a predicted block).
+					  * error_pixel_value = | pixel_in_target_block – corresponding_pixel_in_the matched_block |
+					  */
+					 errorBlockImg = getBestBlockImage(msdWidth,msdHeight,bestBlockImage,blockImg); 
+				}
+			}
+			
+			int motionVectorWidth = width-msdWidth;
+			int motionVectorHeight = height-msdHeight;
+//			System.out.println("motion vector X: " + motionVectorWidth + " motion vector Y: " + motionVectorHeight);
+			
+//			errorImg.display();
+			int[] errorRGB = new int[3];
+			int errorWidth = width;
+			int errorHeight = height;
+			for(int errorW = 0; errorW < n; errorW++) {
+				for(int errorH=0; errorH < n ; errorH++) {
+//					System.out.println("errorW : " + errorW + " errorH : " + errorH);
+//					System.out.println("errorWidth: " + errorWidth + " errorHeight: " + errorHeight);
+						errorBlockImg.getPixel(errorW, errorH, errorRGB);	                  // adding all block error images to the final image.
+						finalImage.setPixel(errorWidth, errorHeight++, errorRGB);
+				}
+				errorWidth++;
+				errorHeight = height;
+			}
+			
 		} // number of blocks ends for the whole image.
 		
 		
+		scaleFinalImage(finalImage);
+		finalImage.display();
 	}
 	
+	 private static void scaleFinalImage(Image finalImage) {
+		
+		 int[] rgb = new int[3];
+		 int[] scalinglist = new int[finalImage.getW()*finalImage.getH()];
+		 int count = 0;
+		 for (int i = 0; i <finalImage.getW(); i++) {
+			for(int j = 0; j<finalImage.getH(); j++) {
+				finalImage.getPixel(i, j, rgb); 
+				scalinglist[count++] = rgb[0];
+			}
+		}
+		 
+		int max = getMax(scalinglist);
+		int min = getMin(scalinglist);
+		System.out.println(max);
+		System.out.println(min);
+		
+		 for (int i = 0; i <finalImage.getW(); i++) {
+				for(int j = 0; j<finalImage.getH(); j++) {
+					finalImage.getPixel(i, j, rgb);
+					if(rgb[0] == max) { rgb[0] = 255; rgb[1]=255; rgb[2]=255; }
+					if(rgb[0] == min) { rgb[0] = 0; rgb[1] = 0; rgb[2] = 0; }
+					finalImage.setPixel(i, j, rgb);
+				}
+			}
+		
+	}
+
+	static Image getBestBlockImage(int bestBlockWidthPosition, int bestBlockHeightPosition, Image bestBlockImage, Image blockImg) {
+		
+		int[] referenceRGB = new int[3];
+		int[] targetRGB = new int[3];
+		int[] errorRGB = new int[3];
+		Image errorImage = new Image(blockImg.getW(),blockImg.getH());
+		
+				for(int w = 0; w < blockImg.getW(); w++) {
+					for(int h = 0; h < blockImg.getH(); h++) {
+					
+						bestBlockImage.getPixel(w, h, referenceRGB);
+						blockImg.getPixel(w, h, targetRGB);
+						int error = Math.abs(targetRGB[0] - referenceRGB[0]);                          // error_pixel_value
+						
+//						System.out.print(error + " ");
+						
+						errorRGB[0] = error; errorRGB[1] = error; errorRGB[2] = error;
+						
+						errorImage.setPixel(w, h, errorRGB);
+					}
+				}
+				
+			return errorImage;
+		}
+	
+	static int calculateTheMSD(Image blockTobeCompared, Image targetBlock) {
+		
+		int sum = 0;
+		int[] rgb = new int[3];
+		int[] rgbTargetImg = new int[3];
+		int m = blockTobeCompared.getW();
+		int n = blockTobeCompared.getH();
+		for(int w = 0; w < m; w++) {
+			for(int h=0 ; h < n; h++) {
+				blockTobeCompared.getPixel(w, h, rgb);
+				targetBlock.getPixel(w, h, rgbTargetImg);
+				
+				sum += (Math.pow(2, (rgbTargetImg[0]-rgb[0]))+
+						Math.pow(2, (rgbTargetImg[1]-rgb[1]))+
+						Math.pow(2, (rgbTargetImg[2]-rgb[2])));
+				
+			}
+		}
+		
+		sum = sum/(m*n);
+		
+		return sum;
+	}
+	
+
+	 /**
+	  * Find the minimum value in a list of all MSD values for a specific Target block.
+	  * @param listOfAllMSDValues
+	  * @return the smallest value of MSD for all the search blocks in the reference image.
+	  */
+	static int findMin(ArrayList<MSD> listOfAllMSDValues) {
+		
+		int[] values = new int[listOfAllMSDValues.size()];
+		for(int i=0; i<listOfAllMSDValues.size(); i++) {
+			values[i] = listOfAllMSDValues.get(i).getValue();
+		}
+		
+		int smallest = values[0];
+		for(int x : values ){
+		   if (x < smallest) {
+		      smallest = x;
+		   }
+		}
+		
+		return smallest;
+	}
 	
 	/** 
 	 *  Extract NxN Blocks from the targetImg
@@ -232,6 +425,26 @@ public class CS4551_Said {
 			
 		}
 		
+		public static int getMax(int[] inputArray){ 
+		    int maxValue = inputArray[0]; 
+		    for(int i=1;i < inputArray.length;i++){ 
+		      if(inputArray[i] > maxValue){ 
+		         maxValue = inputArray[i]; 
+		      } 
+		    } 
+		    return maxValue; 
+		  }
+		 
+		  // Method for getting the minimum value
+		  public static int getMin(int[] inputArray){ 
+		    int minValue = inputArray[0]; 
+		    for(int i=1;i<inputArray.length;i++){ 
+		      if(inputArray[i] < minValue){ 
+		        minValue = inputArray[i]; 
+		      } 
+		    } 
+		    return minValue; 
+		  } 
 		
 	/** 
 	 * 	A method to make the user able to choose an image from project directory.
